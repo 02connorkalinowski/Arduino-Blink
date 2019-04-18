aa878e3fdfc2f809e891c9ac7e52997a05d09d29
Connor Kalinowski <02connor.kalinowski@gulllakecs.org>
2019-04-18 06:54:07 -0700

    Changed HIGH TO 5000

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -25,13 +25,13 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);Type the above text here when the race begins
 }
 
-// the loop function runs over and over again forever
+// the loop function runs over and over aType the above text here when the race beginsgain forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  delay(5000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
